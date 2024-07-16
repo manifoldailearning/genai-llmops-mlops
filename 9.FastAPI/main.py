d1f49d57191061fe57343117df6b462012e499e1
Nachiketh <murthy@manifoldailearning.in>
2024-07-16 07:20:07 +0530

    fastapi-demo

--- 9.FastAPI/main.py
+++ 9.FastAPI/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI 
 from typing import Union
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float 
+    is_offer: bool = None
 
 @app.get("/")
 def display_index():
@@ -14,3 +20,9 @@ def welcome_msg():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def save_item(item_id:int, item: Item):
+    return {"Item_name": item.name,
+            "Item_price": item.price,
+            "Item_id":item_id }
